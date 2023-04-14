3872445da33e202d56836cbfb3f4d5cd02e39a03
Evan L <>
2023-04-14 00:33:40 -0700

    test commit

--- bobcat.c
+++ bobcat.c
@@ -6,4 +6,7 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) { return EXIT_SUCCESS; }
+int main(int argc, char *argv[]) { 
+    print("hello world");
+    return EXIT_SUCCESS;
+}
