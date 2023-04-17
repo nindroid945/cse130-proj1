3af226179b44d7b7b994ae9888d49537ae753a66
Evan L <>
2023-04-17 06:01:32 +0000

    added multi file support

--- bobcat.c
+++ bobcat.c
@@ -14,27 +14,29 @@ void read_from_stdin() {
 }
 
 void read_from_file(char *filename) {
-  int fd, sz;
-    char *c = (char *)calloc(100, sizeof(char));
+    int fd, sz;
+    char c[5000] = "";
 
     fd = open(filename, O_RDONLY);
     if (fd < 0) {
     warn("%s", filename);
-    exit(1);
+    //exit(1);
     }
     sz = read(fd, c, 5000);
     c[sz] = '\0';
     write(STDOUT_FILENO, c, 5000);
-    free(c);
 }
 
 int main(int argc, char *argv[]) {
-  if (argc == 1 ||
-      strcmp(argv[1], "-") == 0) {  // no args, should read from stdin
+    if (argc == 1 ||
+        strcmp(argv[1], "-") == 0) {  // no args, should read from stdin
     read_from_stdin();
     return 0;
-  } else {
-    read_from_file(argv[1]);
-  }
+    } else { //one or more args
+        int i;
+        for(i = 1; i < argc; i++){
+            read_from_file(argv[i]);
+        }
+    }
   return EXIT_SUCCESS;
 }
