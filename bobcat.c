6ddf140168ba6dae07bf5583e7be37f61fe654b8
Evan L <>
2023-04-17 05:37:40 +0000

    one file read working and warn error for no file or dir added

--- bobcat.c
+++ bobcat.c
@@ -13,24 +13,28 @@ void read_from_stdin() {
   }
 }
 
-// void read_from_file(str filename) {
-//   char buffer[5000];
-//   while (read(filename, buffer, 1) > 0) {
-//     printf("%s", buffer);
-//   }
-// }
+void read_from_file(char *filename) {
+  int fd, sz;
+    char *c = (char *)calloc(100, sizeof(char));
+
+    fd = open(filename, O_RDONLY);
+    if (fd < 0) {
+    warn("%s", filename);
+    exit(1);
+    }
+    sz = read(fd, c, 5000);
+    c[sz] = '\0';
+    write(STDOUT_FILENO, c, 5000);
+    free(c);
+}
 
 int main(int argc, char *argv[]) {
   if (argc == 1 ||
       strcmp(argv[1], "-") == 0) {  // no args, should read from stdin
     read_from_stdin();
+    return 0;
   } else {
-    if (strcmp(argv[1], "-") == 0) {
-      // do nothing
-    }
-
-    printf("second arg is %s.\n", argv[2]);
+    read_from_file(argv[1]);
   }
-
   return EXIT_SUCCESS;
 }
