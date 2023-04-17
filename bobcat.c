029cd99b0de7bb583d394e0d8bce97ce62dd2e54
Evan L <>
2023-04-17 02:21:48 +0000

    bobcat w no args implemented

--- bobcat.c
+++ bobcat.c
@@ -6,7 +6,31 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) { 
-    print("hello world");
-    return EXIT_SUCCESS;
+void read_from_stdin() {
+  char buffer[5000];
+  while (read(STDIN_FILENO, buffer, 1) > 0) {
+    printf("%s", buffer);
+  }
+}
+
+// void read_from_file(str filename) {
+//   char buffer[5000];
+//   while (read(filename, buffer, 1) > 0) {
+//     printf("%s", buffer);
+//   }
+// }
+
+int main(int argc, char *argv[]) {
+  if (argc == 1 ||
+      strcmp(argv[1], "-") == 0) {  // no args, should read from stdin
+    read_from_stdin();
+  } else {
+    if (strcmp(argv[1], "-") == 0) {
+      // do nothing
+    }
+
+    printf("second arg is %s.\n", argv[2]);
+  }
+
+  return EXIT_SUCCESS;
 }
